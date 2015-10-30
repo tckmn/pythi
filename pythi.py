98ac16b8ff97b03a72b05648b41083f822ba2630
KeyboardFire <andy@keyboardfire.com>
2015-10-30 17:24:36 -0500

    Make pythi work when called via symlink
    Because Python is weird like that.

--- pythi.py
+++ pythi.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath('./pyth'))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pyth'))
 import pyth
 
 import readline
